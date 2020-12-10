50f63ea3cc9b31a248675970031b1e4c4ea99fb6
cpena <carloshpena93@gmail.com>
2020-12-10 15:47:54 -0500

    built serversReducer

--- frontend/reducers/servers_reducer.js
+++ frontend/reducers/servers_reducer.js
@@ -1,21 +1,28 @@
-//FIX THIS UP
-
-import {RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER} from '../actions/session';
+import {RECEIVE_SERVER, RECEIVE_SERVERS, CLEAR_SERVER} from '../actions/server';
 
 
 
-const usersReducer = (state = {}, action) => {
+const serversReducer = (state = {}, action) => {
   Object.freeze(state);
+  let nextState = Object.assign({}, state);
   switch(action.type){
-    case RECEIVE_CURRENT_USER:
-      // return Object.assign({}, state, { id: action.user});
+    case RECEIVE_SERVERS: // a channel object looks like {"2":{"id":2,"channelTitle":"demoChan","serverId":7}
+      return Object.assign({}, state, action.servers); 
+      // example: will look like
+      // { 
+      //  '2': { id: 2, channelTitle: 'demoChan', serverId: 7 },
+      // '3': { id: 3, channelTitle: 'demoChan2', serverId: 7 } 
+      //}
       // return {hello: "goodbye" };
-      return { id: action.user};
-    case LOGOUT_CURRENT_USER:
-      return {};
+      // return { id: action.user};
+    case RECEIVE_SERVER:
+      return Object.assign({}, state, action.server);
+    case CLEAR_SERVER:
+      delete nextState[`${action.serverId}`]
+      return nextState;
     default:
       return state;
   }
 }
 
-export default usersReducer;
+export default serversReducer;
