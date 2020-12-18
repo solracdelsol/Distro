2ec6f43604c0f6fee5384aff24c507ea6d19c2fc
cpena <carloshpena93@gmail.com>
2020-12-18 16:19:55 -0500

    built message util file

--- app/views/api/messages/_message.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! message, :id, :body, :user_id, :channel_id, :created_at, :updated_at
-json.url message_url(message, format: :json)
---
+++ frontend/util/message.js
@@ -0,0 +1,21 @@
+export const postMessage = (formData) =>( //take channelId straight from formData instead of adding a serverId as a second argument 
+  $.ajax({
+    url: `/api/channels/${formData.channelId}/messages`,
+    method: "POST",
+    data: formData
+  })
+)
+
+export const getMessage = (messageObj) => (
+  $.ajax({
+    url: `api/channels/${messageObj.channelId}/messages/${messageObj.id}`,
+    method: "GET",
+  })
+);
+
+export const getMessages = (messageObj) => ( //gets all messages of a given channel
+  $.ajax({
+    url: `api/channels/${messageObj.channelId}/messages`,
+    method: "GET",
+  })
+);
